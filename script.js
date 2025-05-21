81c673dceed924ab45123d403e052159c23beacc
lorenzete1 <lorenzete@proton.me>
2025-05-21 17:34:48 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
+import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm'
+
+const supabaseUrl = 'https://fmhnzooghyfltnkjiwzf.supabase.co'
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZtaG56b29naHlmbHRua2ppd3pmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDc4Mzg3ODcsImV4cCI6MjA2MzQxNDc4N30.-5zCQNWfj9BiME2NSIwtAocIvvNn8NvY1f0CKwmeFrA'
+
+const supabase = createClient(supabaseUrl, supabaseKey)
 
 const supabase = supabase.createClient(
   'https://fmhnzooghyfltnkjiwzf.supabase.co',
